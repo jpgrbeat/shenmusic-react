369c5c188bdc914535c2eb9bfa4706ba9239665c
Josh <joshua.ross.miller@gmail.com>
2018-12-11 16:16:04 -0500

    more styling

---
+++ src/components/Descriptors.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export const Sales =()=>{
+  return(
+    <div>
+      <h3>Sales</h3>
+      <p></p>
+    </div>
+  )
+}
+export const Service = ()=>{
+  return(
+    <div>
+      <h3> Service and Repairs</h3>
+      <p></p>
+    </div>
+  )
+}
+export const Lessons = ()=>{
+  return(
+    <div>
+      <h3>Lessons</h3>
+      <p></p>
+    </div>
+  )
+}
+export const Rentals = ()=>{
+  return(
+    <div>
+      <h3>Rentals</h3>
+      <p></p>
+    </div>
+  )
+}
--- src/components/ServiceDescriptor.js
+++ src/components/ServiceDescriptor.js
@@ -3,20 +3,65 @@ import Drum from '../images/perump.jpg'
 import Staff from '../images/staff-sales-image.jpg'
 import Snow from '../images/snow-shen.jpg'
 import Ties from '../images/tie-fridays.jpg'
-const ServiceDescriptor =()=>{
-  return(
-    <div className='service-container'>
-      <div className='service-gallery'>
-        <div className='row'>
-          <img src={Ties} alt='tie fridays'/>
-          <img src={Snow} alt='snow'/>
+import {Sales,Service,Lessons,Rentals} from './Descriptors'
+class ServiceDescriptor extends React.Component{
+  state={
+    selectorId:null
+  }
+  onClick=(e)=>{
+    if(this.state.selectorId === e.target.id){
+      this.setState({
+        selectorId:null
+      })
+    }else{
+      this.setState({
+        selectorId: e.target.id
+      })
+    }
+  }
+  render(){
+    let {selectorId} = this.state;
+    return(
+      <div className='service-container'>
+        <div className='service-gallery'>
+          <div className='row-g'>
+            <div className='col'>
+              <img src={Ties} alt='ties'/>
+              <div className='image-overlay' id='Sales' onClick={this.onClick}>
+                <h3>Sales</h3>
+              </div>
+            </div>
+            <div className='col'>
+              <img src={Drum} alt='ties'/>
+              <div className='image-overlay' id='Service' onClick={this.onClick}>
+                <h3>Service and Repairs</h3>
+              </div>
+            </div>
+          </div>
+          <div className='row-g'>
+            <div className='col'>
+              <img src={Snow} alt='ties'/>
+              <div className='image-overlay' id='Rentals' onClick={this.onClick}>
+                <h3>Rentals</h3>
+              </div>
+
+            </div>
+            <div className='col' >
+              <img src={Staff} alt='ties'/>
+              <div className='image-overlay' id='Lessons' onClick={this.onClick}>
+                <h3>Lessons</h3>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className='row'>
-          <img src={Drum} alt='drum'/>
-          <img src={Staff} alt='staff'/>
+        <div className="row">
+          {selectorId === 'Sales' ? <Sales/> : null}
+          {selectorId === 'Service' ? <Service/> :null}
+          {selectorId === 'Lessons' ? <Lessons/> : null}
+          {selectorId === 'Rentals' ? <Rentals/> : null}
         </div>
       </div>
-    </div>
-  )
+    )
+}
 }
 export default ServiceDescriptor
