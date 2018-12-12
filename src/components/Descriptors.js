477b11bc3a4b9ddbc11563689666976ac862aae1
Josh <joshua.ross.miller@gmail.com>
2018-12-12 11:41:12 -0500

    switched branches for different styles to give options

--- src/components/Descriptors.js
+++ src/components/Descriptors.js
@@ -4,7 +4,7 @@ export const Sales =()=>{
   return(
     <div>
       <h3>Sales</h3>
-      <p></p>
+      <p>Lorem ipsum dolor sit amet, nostrud iuvaret petentium cum te. Agam expetenda gubergren sit ex, eos ne lorem aeterno consectetuer. Ne quo odio quas! Ex nam platonem scripserit, eam eu vocent repudiare. His ut doming dictas urbanitas, aeterno pertinacia assueverit mei cu!</p>
     </div>
   )
 }
@@ -12,7 +12,7 @@ export const Service = ()=>{
   return(
     <div>
       <h3> Service and Repairs</h3>
-      <p></p>
+      <p>Lorem ipsum dolor sit amet, nostrud iuvaret petentium cum te. Agam expetenda gubergren sit ex, eos ne lorem aeterno consectetuer. Ne quo odio quas! Ex nam platonem scripserit, eam eu vocent repudiare. His ut doming dictas urbanitas, aeterno pertinacia assueverit mei cu!</p>
     </div>
   )
 }
@@ -20,7 +20,7 @@ export const Lessons = ()=>{
   return(
     <div>
       <h3>Lessons</h3>
-      <p></p>
+      <p>Lorem ipsum dolor sit amet, nostrud iuvaret petentium cum te. Agam expetenda gubergren sit ex, eos ne lorem aeterno consectetuer. Ne quo odio quas! Ex nam platonem scripserit, eam eu vocent repudiare. His ut doming dictas urbanitas, aeterno pertinacia assueverit mei cu!</p>
     </div>
   )
 }
@@ -28,7 +28,7 @@ export const Rentals = ()=>{
   return(
     <div>
       <h3>Rentals</h3>
-      <p></p>
+      <p>Lorem ipsum dolor sit amet, nostrud iuvaret petentium cum te. Agam expetenda gubergren sit ex, eos ne lorem aeterno consectetuer. Ne quo odio quas! Ex nam platonem scripserit, eam eu vocent repudiare. His ut doming dictas urbanitas, aeterno pertinacia assueverit mei cu!</p>
     </div>
   )
 }
